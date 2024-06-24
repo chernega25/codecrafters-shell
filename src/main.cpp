09dd8f4d69cd026d239de075e7a5f5a6c5ad9d00
chernega25 <cherneganikita@gmail.com>
2024-06-24 15:25:36 +0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> split_message(const std::string &message, const std::string& delim) {
+    std::vector<std::string> toks;
+    std::stringstream ss = std::stringstream(message);
+    std::string line;
+    while (getline(ss, line, *delim.begin())) {
+        toks.push_back(line);
+        ss.ignore(delim.length() - 1);
+    }
+    return toks;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,4 +24,8 @@ int main() {
   
   std::string input;
   std::getline(std::cin, input);
+
+  auto commands = split_message(input, " ");
+  std::cout << commands[0] << ": command not found" << std::endl;
+
 }
