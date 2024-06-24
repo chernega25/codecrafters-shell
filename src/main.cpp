df9409da3d7b75ffcdc833e5407e04d5393d182a
chernega25 <cherneganikita@gmail.com>
2024-06-24 16:51:44 +0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <sstream>
 
-std::vector<std::string> split_message(const std::string &message, const std::string& delim) {
-    std::vector<std::string> toks;
-    std::stringstream ss = std::stringstream(message);
-    std::string line;
-    while (getline(ss, line, *delim.begin())) {
-        toks.push_back(line);
-        ss.ignore(delim.length() - 1);
-    }
-    return toks;
-}
+enum shell_command {
+    exit0,
+    type,
+    echo
+};
 
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+
+    std::unordered_map<std::string, shell_command> commands;
+    commands.insert({"exit", exit0}); 
+    commands.insert({"type", type}); 
+    commands.insert({"echo", echo});
 
     while(true) {
         std::cout << "$ ";
@@ -26,17 +27,38 @@ int main() {
         std::getline(std::cin, input);
         
         std::stringstream ss = std::stringstream(input);
-        std::string command;
-        std::getline(ss, command, ' ');
+        std::string str_command;
+        std::getline(ss, str_command, ' ');
+        std::string message;
+        std::getline(ss, message);
 
-        if (command == "exit") {
-            return 0;
-        } else if (command == "echo") {
-            std::string message;
-            std::getline(ss, message);
-            std::cout << message << std::endl;
-        } else {
-            std::cout << command << ": command not found\n";
+        if (!commands.contains(str_command)) {
+            std::cout << str_command << ": command not found\n";
+            continue;
+        }
+
+        auto command = commands[str_command];
+        
+        switch (command) {
+
+            case exit0: {
+                return 0;
+                break;
+            };
+
+            case type: {
+                if (commands.contains(message)) {
+                    std::cout << message << " is a shell builtin\n";
+                } else {
+                    std::cout << message << ": not found\n";
+                }
+                break;
+            };
+
+            case echo: {
+                std::cout << message << std::endl;
+                break;
+            };
         }
     }
 }
